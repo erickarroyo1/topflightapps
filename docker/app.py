22386a1a8e3fb3eb25ed55b6ad75173ffc737387
Erick Ernesto Arroyo Viera <50220864+erickarroyo1@users.noreply.github.com>
2023-08-30 19:04:37 -0500

    edit flask app to avoid /console due security reasons

--- docker/app.py
+++ docker/app.py
@@ -32,4 +32,4 @@ def hello_world():
     return greeting
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, use_evalex=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
